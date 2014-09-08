b1ba8e1e52825a3e5a362aaf8910ac898c66f29d
marcel <admin@moerbst.de>
2014-09-08 10:01:54 +0200

    Update sili_gw01_reconnect.sh

--- sili_gw01_reconnect.sh
+++ sili_gw01_reconnect.sh
@@ -9,8 +9,8 @@ USER="root"
 PW="YOURPASSWD"
 ADDRESS="10.42.11.254"
 
-echo "disconnection ..."
+echo "disconnecting ..."
 curl -d interface=wan -d submit=Disconnect --basic https://$USER:$PW@$ADDRESS/status_interfaces.php > /dev/null 2>&1
 sleep 3
-echo "connection ..."
+echo "reconnecting ..."
 curl -d interface=wan -d submit=Connect --basic https://$USER:$PW@$ADDRESS/status_interfaces.php > /dev/null 2>&1
